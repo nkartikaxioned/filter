2e036d32b9a0c6006ca8dcea9628e7a1c81a5dcf
nkartikaxioned <kartikn@axioned.com>
2023-10-10 10:44:25 +0530

    js functionality 100% add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,37 @@
 const button = document.querySelectorAll('.btn'),
- filterDiv = document.querySelectorAll('.filterDiv'),
- wallpapers =document.querySelectorAll('.wallpaper'),
- bussiness = document.querySelectorAll('.bussiness'),
- stationerys = document.querySelectorAll('.stationery'),
- websites = document.querySelectorAll('.website'),
- mobiles = document.querySelectorAll('.mobile');
+  filterDiv = document.querySelectorAll('.filterDiv'),
+  wallpapers = document.querySelectorAll('.wallpaper'),
+  bussiness = document.querySelectorAll('.bussiness'),
+  stationerys = document.querySelectorAll('.stationery'),
+  websites = document.querySelectorAll('.website'),
+  mobiles = document.querySelectorAll('.mobile');
 
- button.forEach(btn => {
-    btn.addEventListener('click', () => {
-        removeactive();
-        btn.classList.add('active');
-        let dataFilter=btn.getAttribute('data-filter');
-        console.log(dataFilter);
-        filterDiv.forEach(filter => {
-            console.log(filter.getAttribute('data-item'))
-            // removedisplaytrue();
-          if(filter.getAttribute('data-item') == dataFilter){
-            
-            filter.classList.add('displaytrue');
-          }
-        });
-    })
+button.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    removeactive();
+    btn.classList.add('active');
+    let dataFilter = btn.getAttribute('data-filter');
+     
+    filterDiv.forEach(filter => {
+      filter.classList.remove('displaytrue');
+      filter.classList.add('displaynone');
+      // condition satisfied then following class will be added and removed 
+      if (filter.getAttribute('data-item') == dataFilter || dataFilter == 'all') {
+
+        filter.classList.add('displaytrue');
+        filter.classList.remove('dispalynone');
+      }
+    });
+  })
 });
 
-function removedisplaytrue(){
-    filterDiv.forEach(div => {
-        div.classList.remove('displaytrue');
-    })
-};
-
+//function to remove active class from nav
 function removeactive() {
-    button.forEach(btn => {
-        btn.classList.remove('active');
-    })
+  button.forEach(btn => {
+    btn.classList.remove('active');
+  })
 };
-// function filter(element){
-//     console.log(element)
-//     if(element === 'wallpaper') {
-//         remove()
-//         deactivate();
-//      wallpapers.forEach((wallpaper) => {
-//         wallpaper.classList.add('displaytrue');
-//         console.log(element)
-//      })
-//     }
-    
-//     if(element === 'bussiness') {
-//         console.log(element)
-//         remove()
-//         deactivate()
-//         bussiness.forEach((bussines) => {
-//             bussines.classList.add('displaytrue');
-//          })
-//     }
-    
-//     if(element === 'stationery') {
-//         console.log(element)
-//         remove()
-//         deactivate()
-//         stationerys.forEach((stationery) => {
-//             stationery.classList.add('displaytrue');
-//          })
-//     }
-   
-//     if(element === 'website') {
-//         console.log(element)
-//         remove()
-//         deactivate()
-//         websites.forEach((website) => {
-//             website.classList.add('displaytrue');
-//          })
-//     }
-    
-//     if(element === 'mobile') {
-//         console.log(element)
-//         remove()
-//         deactivate()
-//         mobiles.forEach((mobile) => {
-//             mobile.classList.add('displaytrue');
-//          })
-//     }
-    
-//     if(element === 'all') {
-//         console.log(element)
-//         remove()
-//         deactivate()
-//         filterDiv.forEach((filter) => {
-//             filter.classList.add('displaytrue')
-//         })
-//     }
 
-//  };
-
-// function remove(){
-//     filterDiv.forEach((filter) => {
-//         filter.classList.remove('displaynone');
-//     })
-// }
-
-// function deactivate() {
-//     filterDiv.forEach((filter) => {
-//         filter.classList.remove('displaytrue');
-//         filter.classList.add('displaynone');
-//     })
-// }
 
 
 
